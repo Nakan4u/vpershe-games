38f2b69a9c7a51df11d05774dec1ba373b2afdc9
Grygorii Nakonechnyi <nakan88@gmail.com>
2019-04-07 00:19:36 +0300

    add draft intro page

--- your-gender/src/App.js
+++ your-gender/src/App.js
@@ -5,11 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Vpershe games app</h1>
+      <div className="wrapper">
+        <main>
+          <h1>Vpershe games app</h1>
+          <img src="https://vpershe.com/assets/uploads/illustration_sex.png" height="300"></img>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          <button>Play</button>
+        </main>
         <footer>
           <a href="https://vpershe.com/" target="blank">
-            <img src={logo} alt="logo"></img>
+            <img src={logo} alt="logo" width="113"></img>
           </a>
         </footer>
       </div>
