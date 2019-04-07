9caff6235e47159ac55032b11df6992e579630f0
Grygorii Nakonechnyi <nakan88@gmail.com>
2019-04-07 18:20:50 +0300

    Updates

---
+++ precache-manifest.c5637ba3010eb10b781d5d20134e252a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d960c503c057e59d94b65b0315781c4e",
+    "url": "/vpershe-games/static/media/Illustration-1.d960c503.png"
+  },
+  {
+    "revision": "f3463aae079c1f77a5d6",
+    "url": "/vpershe-games/static/css/main.197da1d6.chunk.css"
+  },
+  {
+    "revision": "1b93204253cac2da9de4",
+    "url": "/vpershe-games/static/js/runtime~main.bdd25a8e.js"
+  },
+  {
+    "revision": "4ba6f791157a3d861d9d",
+    "url": "/vpershe-games/static/js/2.d1c1a038.chunk.js"
+  },
+  {
+    "revision": "9245e4f09937ba18379cac1d40f8b823",
+    "url": "/vpershe-games/static/media/flag-start.9245e4f0.svg"
+  },
+  {
+    "revision": "1a1d68f15918a56dd5c4aaa1466d11df",
+    "url": "/vpershe-games/static/media/flag-finish.1a1d68f1.svg"
+  },
+  {
+    "revision": "79a41f7386454e698369ceb61d21fb94",
+    "url": "/vpershe-games/static/media/Illustration-main.79a41f73.png"
+  },
+  {
+    "revision": "f3463aae079c1f77a5d6",
+    "url": "/vpershe-games/static/js/main.1ed23d0e.chunk.js"
+  },
+  {
+    "revision": "13128a80b2839ae42232749c18809460",
+    "url": "/vpershe-games/static/media/logo.13128a80.svg"
+  },
+  {
+    "revision": "c9f8efbec85754e3f90c7b560849b441",
+    "url": "/vpershe-games/static/media/Illustration-3.c9f8efbe.png"
+  },
+  {
+    "revision": "7a8997ab0250d04b6228b30da9498233",
+    "url": "/vpershe-games/static/media/arrow.7a8997ab.svg"
+  },
+  {
+    "revision": "19c965efc0af0a3c198aba4c6f01d8bf",
+    "url": "/vpershe-games/static/media/Illustration-final.19c965ef.png"
+  },
+  {
+    "revision": "1a7e787975327d64558527dbbab21130",
+    "url": "/vpershe-games/static/media/button-background.1a7e7879.svg"
+  },
+  {
+    "revision": "a2912498afd28a0f97c92a596d24601e",
+    "url": "/vpershe-games/static/media/button-background-hover.a2912498.svg"
+  },
+  {
+    "revision": "21c36044177755cd26ae4c8560157cbd",
+    "url": "/vpershe-games/index.html"
+  }
+];
